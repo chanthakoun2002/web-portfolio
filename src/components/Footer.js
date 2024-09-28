5476be8a1e810c924905cbe47ed5dc771ebd5121
chanthakoun2002 <chanthakounandrew@gmail.com>
2024-09-28 14:26:09 -0700

    changes to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,11 +7,11 @@ const Footer = () => (
     <p>Made by Andrew Chanthakoun © 2024</p>
     <div className="social-links">
     {/* <h3>Contact Me</h3> */}
-      <a href="mailto: Chanthakounandrew@gmail.com">ChanthakounAndrew@gmail.com</a>
+      {/* <a href="mailto: Chanthakounandrew@gmail.com">ChanthakounAndrew@gmail.com</a>
       <span> | </span>
       <a href="https://www.linkedin.com/in/andrew-chanthakoun-0a94961a1/">LinkedIn</a>
       <span> | </span>
-      <a href="https://github.com/chanthakoun2002">GitHub</a>
+      <a href="https://github.com/chanthakoun2002">GitHub</a> */}
     </div>
   </footer>
 );
